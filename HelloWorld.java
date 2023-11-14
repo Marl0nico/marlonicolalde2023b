92f4c1b1afa3d774ee574003d1e922d4eb254a84
Marl0nico <marlon.nicolalde@epn.edu.ec>
2023-11-14 16:46:57 -0500

    Arch.java
    Prueba2

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld{
 		System.out.println("me uno al grupo Otra versión diferente +++");
 		System.out.println("me uno al grupo Fran");
 		System.out.println("Probando nuevos cambios");
+		System.out.println("Más y más pruebas");
 	}
 }
