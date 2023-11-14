6821ca88864a63bc3e49bc2430bcd11df4cd1399
Juan Pablo Zaldumbide <jzaldumbide@gmail.com>
2023-11-14 09:41:43 -0500

    Update HelloWorld.java
    me uno al grupo

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("Otra versión diferente +++");
+		System.out.println("me uno al grupo Otra versión diferente +++");
 	}
 }
