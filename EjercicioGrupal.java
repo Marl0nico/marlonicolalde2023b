ec0fec7f8f7a992101404ec8a897147b326a177f
Marl0nico <150805769+Marl0nico@users.noreply.github.com>
2023-11-16 19:55:25 -0500

    Add files via upload

---
+++ EjercicioGrupal.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class EjercicioGrupal{
+    public static void main(String[] args) {
+        Scanner ejercicio=new Scanner(System.in);
+        System.out.print("Ingrese el primer nombre: ");
+        String n1=ejercicio.nextLine();
+        System.out.print("Usted se llama "+n1);
+    }
+}
